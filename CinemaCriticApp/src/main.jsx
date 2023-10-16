2ac595e6a6000f11b48d210f9e2654a4dbd5f05d
Mario <mario.vazquez.otero@gmail.com>
2023-10-16 17:14:11 +0200

    Added missing implementation of the LazyApp component

--- CinemaCriticApp/src/main.jsx
+++ CinemaCriticApp/src/main.jsx
@@ -1,15 +1,14 @@
 import { BrowserRouter } from 'react-router-dom'
 import ReactDOM from 'react-dom/client'
-import App from './App'
+import LazyApp from './App'
 import './stylesheets/index.css'
 import { store } from './store/store'
 import { Provider } from 'react-redux'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  // TODO: lazy routes
   <Provider store={store}>
     <BrowserRouter>
-      <App />
+      <LazyApp />
     </BrowserRouter>
   </Provider>
 )
